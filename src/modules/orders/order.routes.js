831960f342364e2026eaf739d610988641a66d19
3b8ny <ahmedroute5@gmail.com>
2024-09-03 17:23:00 +0300

    dasd

--- src/modules/orders/order.routes.js
+++ src/modules/orders/order.routes.js
@@ -3,13 +3,14 @@ import * as OC from "./order.controller.js";
 import { auth } from "../../middleware/auth.js";
 import { validation } from "../../middleware/validation.js";
 import * as CAC from "./order.validation.js";
+import { roles } from "../../utils/roles.js";
 
 const orderRouter = Router()
 
 
 orderRouter.post("/",
     // validation(CAC.createOrder),
-    auth(),
+    auth(Object.values(roles)),
     OC.createOrder)
 
 orderRouter.patch("/:orderId",
